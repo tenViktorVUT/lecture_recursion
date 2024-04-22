c2f56c4de9d171b19a1f783abdd9cf8787138630
Viktor Morovic <257026@vutbr.cz>
2024-04-22 12:10:50 +0200

    fibonacci sequence recursive func

--- fibo.py
+++ fibo.py
@@ -1,6 +1,14 @@
+def fib_sequence(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib_sequence(n-1) + fib_sequence(n-2)
+
 
 def main():
-    pass
+    print(fib_sequence(4))
 
 
 if __name__ == "__main__":
